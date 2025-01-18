a116e184c2c4428bf454ca602b021f1c4d39d0a6
Olga <o.gavrikova877@gmail.com>
2025-01-18 12:34:26 +0400

    add brain-even

--- brain_games/module_even.py
+++ brain_games/module_even.py
@@ -1,45 +1,39 @@
 from random import randint
-
+import prompt
 
 
 def is_even():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     message = 'Correct!'
     x = randint(1, 20)
     print(f'Question: {x}')
-    y = input()
+    y = prompt.string('Your answer: ')
     i = 1
-    print(f'Your answer: {y}')
     if (x % 2 == 0 and y == 'yes') or (x % 2 != 0 and y == 'no'):
-        while (i < 3) and ((x % 2 == 0 and y == 'yes') or (x % 2 != 0 and y == 'no')):
-            print(message)
-            x = randint(1, 20)
-            print(f'Question: {x}')
-            y = input()
-            i+=1 
-        print('Congratulations, name!')
+        print(message)
+        x = randint(1, 20)
+        print(f'Question: {x}')
+        y = prompt.string('Your answer: ')
+    if (x % 2 == 0 and y == 'yes') or (x % 2 != 0 and y == 'no'):
+        print(message)
+        x = randint(1, 20)
+        print(f'Question: {x}')
+        y = prompt.string('Your answer: ')
+    if (x % 2 == 0 and y == 'yes') or (x % 2 != 0 and y == 'no'):
+        print(message)
+        print(f'Congratulations, {name}!')
     elif x % 2 == 0 and y == 'no':
-            message = "'no' is wrong answer ;(. Correct answer was 'yes'./n Let's try again, Bill!"
+            # y = prompt.string('Your answer: ')
+            message = f"'no' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, {name}!"
             print(message)
     elif x % 2 != 0 and y == 'yes':
-            message = "'yes' is wrong answer ;(. Correct answer was 'no'./n Let's try again, Bill!"
+            # y = prompt.string('Your answer: ')
+            # print(f'Your answer: {y}')
+            message = f"'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, {name}!"
             print(message)
     
     else:
         print('Try again')
-
-
-    
-
-# def even():
-#     print('Answer "yes" if the number is even, otherwise answer "no".')
-#     # message = 'Correct!'
-#     i = 1
-#     while (i <= 3) and (message == 'Correct!'):
-#         is_even()
-#         i+=1
-#     print('Congratulations, name!')
-
-
-
-is_even()
 
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+from brain_games.module_even import is_even
+
+
+
+def main():
+    is_even()
+
+
+
+if __name__ == "__main__":
+    main()
