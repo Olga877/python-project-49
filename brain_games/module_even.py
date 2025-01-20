1c01562a1a263d389dd7bafc67268c131b2c3b10
Olga <o.gavrikova877@gmail.com>
2025-01-20 09:38:08 +0400

    fixed brain-even

--- brain_games/module_even.py
+++ brain_games/module_even.py
@@ -11,29 +11,20 @@ def is_even():
     print(f'Question: {x}')
     y = prompt.string('Your answer: ')
     i = 1
-    if (x % 2 == 0 and y == 'yes') or (x % 2 != 0 and y == 'no'):
-        print(message)
-        x = randint(1, 20)
-        print(f'Question: {x}')
-        y = prompt.string('Your answer: ')
-    if (x % 2 == 0 and y == 'yes') or (x % 2 != 0 and y == 'no'):
-        print(message)
-        x = randint(1, 20)
-        print(f'Question: {x}')
-        y = prompt.string('Your answer: ')
-    if (x % 2 == 0 and y == 'yes') or (x % 2 != 0 and y == 'no'):
-        print(message)
-        print(f'Congratulations, {name}!')
-    elif x % 2 == 0 and y == 'no':
-            # y = prompt.string('Your answer: ')
-            message = f"'no' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, {name}!"
-            print(message)
-    elif x % 2 != 0 and y == 'yes':
-            # y = prompt.string('Your answer: ')
-            # print(f'Your answer: {y}')
-            message = f"'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, {name}!"
-            print(message)
     
-    else:
-        print('Try again')
+    if (x % 2 == 0 and y == 'yes') or (x % 2 != 0 and y == 'no'):
+            while i < 3:
+                print(message)
+                x = randint(1, 20)
+                print(f'Question: {x}')
+                y = prompt.string('Your answer: ')
+                i += 1
+                print(message)
+                print(f'Congratulations, {name}!')
+    if x % 2 == 0 and y != 'yes':
+            message = f"'{y}' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, {name}!"
+            print(message)
+    elif x % 2 != 0 and y != 'no':
+            message = f"'{y}' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, {name}!"
+            print(message)
 
