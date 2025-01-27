4cf5ff0df3a5d5781a0d3ecc77849e18e6a4010a
Olga <o.gavrikova877@gmail.com>
2025-01-27 09:04:53 +0400

    add brain-progression

--- Makefile
+++ Makefile
@@ -8,6 +8,8 @@ brain-calc:
 	uv run brain-calc
 brain-gcd:
 	uv run brain-gcd
+brain-progression:
+	uv run brain-progression
 build:
 	uv build
 package-install:
---
+++ brain_games/games/module_progression.py
@@ -0,0 +1,44 @@
+import random
+import prompt
+
+start_number = random.randint(1, 100)
+step = random.randint(1, 5)
+progression_length = random.randint(5, 10)
+progression_list = []
+name = prompt.string('May I have your name? ')
+
+def run_engine():
+   iterations_number = 3
+   for _ in range(iterations_number):
+      final_list = get_progression()
+      
+      missing_number = random.choice(final_list)
+      index_of_missing_number = final_list.index(missing_number)
+      final_list[index_of_missing_number] = '..'
+      correct_answer = str(missing_number)
+      question = f'Question: {final_list}'
+
+      print(question)
+      answer = prompt.string('Your answer: ')
+      if answer.lower() == correct_answer:
+            print("Correct!")
+      else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\n Let's try again, {name}!")
+            return
+   print(f'Congratulations, {name}!')
+
+
+def get_progression():
+ for i in range(progression_length):
+    progression_list.append(start_number + (i-1)*step)
+ return progression_list
+
+
+def run_progression_game():
+    print(f'Hello, {name}!')
+    print('What number is missing in the progression?')
+   
+    run_engine()
+ 
+
+
